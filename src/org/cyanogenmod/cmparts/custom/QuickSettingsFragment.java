da16b88d93b9c4b4a4783a5b5d816fbf410b41ee
Ali B <abittin@gmail.com>
2016-11-30 22:42:53 +0200

    CMParts: SystemUI: QS tile titles visibility
      * See FWB part for description
    
    Change-Id: I59f0bc2e5629ef163a1de053cd4cffdfe3387e16

--- src/org/cyanogenmod/cmparts/custom/QuickSettingsFragment.java
+++ src/org/cyanogenmod/cmparts/custom/QuickSettingsFragment.java
@@ -126,94 +126,105 @@ public class SettingsPreferenceFragment extends PreferenceFragment
         mSysuiQqsCount.setSummary(mSysuiQqsCount.getEntry());
         mSysuiQqsCount.setOnPreferenceChangeListener(this);
 
-        @Override
-        public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-            return super.onPreferenceTreeClick(preferenceScreen, preference);
-        }
+        mQsTileTitleVisibility = (SwitchPreference) findPreference(PREF_QS_TILE_TITLE_VISIBILITY);
+        mQsTileTitleVisibility.setOnPreferenceChangeListener(this);
+        mQsTileTitleVisibility.setChecked((Settings.System.getIntForUser(resolver,
+                Settings.System.QS_TILE_TITLE_VISIBILITY, 0,
+                UserHandle.USER_CURRENT) == 1));
+    }
 
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object newValue) {
-            ContentResolver resolver = getActivity().getContentResolver();
-            int intValue;
-            int index;
-            if (preference == mTileAnimationStyle) {
-                int tileAnimationStyle = Integer.parseInt((String) newValue);
-                Settings.System.putIntForUser(resolver, Settings.System.ANIM_TILE_STYLE,
-                        tileAnimationStyle, UserHandle.USER_CURRENT);
-                updateTileAnimationStyleSummary(tileAnimationStyle);
-                updateAnimTileStyle(tileAnimationStyle);
-                return true;
-            } else if (preference == mTileAnimationDuration) {
-                int tileAnimationDuration = Integer.parseInt((String) newValue);
-                Settings.System.putIntForUser(resolver, Settings.System.ANIM_TILE_DURATION,
-                        tileAnimationDuration, UserHandle.USER_CURRENT);
-                updateTileAnimationDurationSummary(tileAnimationDuration);
-                return true;
-            } else if (preference == mTileAnimationInterpolator) {
-                int tileAnimationInterpolator = Integer.parseInt((String) newValue);
-                Settings.System.putIntForUser(resolver, Settings.System.ANIM_TILE_INTERPOLATOR,
-                        tileAnimationInterpolator, UserHandle.USER_CURRENT);
-                updateTileAnimationInterpolatorSummary(tileAnimationInterpolator);
-                return true;
-            } else if (preference == mRowsPortrait) {
-                intValue = Integer.parseInt((String) newValue);
-                index = mRowsPortrait.findIndexOfValue((String) newValue);
-                Settings.Secure.putInt(resolver,
-                        Settings.Secure.QS_ROWS_PORTRAIT, intValue);
-                preference.setSummary(mRowsPortrait.getEntries()[index]);
-                return true;
-            } else if (preference == mRowsLandscape) {
-                intValue = Integer.parseInt((String) newValue);
-                index = mRowsLandscape.findIndexOfValue((String) newValue);
-                Settings.Secure.putInt(resolver,
-                        Settings.Secure.QS_ROWS_LANDSCAPE, intValue);
-                preference.setSummary(mRowsLandscape.getEntries()[index]);
-                return true;
-            } else if (preference == mQsColumns) {
-                intValue = Integer.parseInt((String) newValue);
-                index = mQsColumns.findIndexOfValue((String) newValue);
-                Settings.Secure.putInt(resolver,
-                        Settings.Secure.QS_COLUMNS, intValue);
-                preference.setSummary(mQsColumns.getEntries()[index]);
-                return true;
-            } else if (preference == mSysuiQqsCount) {
-                String SysuiQqsCount = (String) newValue;
-                int SysuiQqsCountValue = Integer.parseInt(SysuiQqsCount);
-                Settings.Secure.putInt(resolver, Settings.Secure.QQS_COUNT, SysuiQqsCountValue);
-                int SysuiQqsCountIndex = mSysuiQqsCount.findIndexOfValue(SysuiQqsCount);
-                mSysuiQqsCount.setSummary(mSysuiQqsCount.getEntries()[SysuiQqsCountIndex]);
-                return true;
-            }
-            return false;
-        }
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        return super.onPreferenceTreeClick(preferenceScreen, preference);
+    }
 
-        private void updateTileAnimationStyleSummary(int tileAnimationStyle) {
-            String prefix = (String) mTileAnimationStyle.getEntries()[mTileAnimationStyle.findIndexOfValue(String
-                    .valueOf(tileAnimationStyle))];
-            mTileAnimationStyle.setSummary(getResources().getString(R.string.qs_set_animation_style, prefix));
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        int intValue;
+        int index;
+        if (preference == mTileAnimationStyle) {
+            int tileAnimationStyle = Integer.parseInt((String) newValue);
+            Settings.System.putIntForUser(resolver, Settings.System.ANIM_TILE_STYLE,
+                    tileAnimationStyle, UserHandle.USER_CURRENT);
+            updateTileAnimationStyleSummary(tileAnimationStyle);
+            updateAnimTileStyle(tileAnimationStyle);
+            return true;
+        } else if (preference == mTileAnimationDuration) {
+            int tileAnimationDuration = Integer.parseInt((String) newValue);
+            Settings.System.putIntForUser(resolver, Settings.System.ANIM_TILE_DURATION,
+                    tileAnimationDuration, UserHandle.USER_CURRENT);
+            updateTileAnimationDurationSummary(tileAnimationDuration);
+            return true;
+        } else if (preference == mTileAnimationInterpolator) {
+            int tileAnimationInterpolator = Integer.parseInt((String) newValue);
+            Settings.System.putIntForUser(resolver, Settings.System.ANIM_TILE_INTERPOLATOR,
+                    tileAnimationInterpolator, UserHandle.USER_CURRENT);
+            updateTileAnimationInterpolatorSummary(tileAnimationInterpolator);
+            return true;
+        } else if (preference == mRowsPortrait) {
+            intValue = Integer.parseInt((String) newValue);
+            index = mRowsPortrait.findIndexOfValue((String) newValue);
+            Settings.Secure.putInt(resolver,
+                    Settings.Secure.QS_ROWS_PORTRAIT, intValue);
+            preference.setSummary(mRowsPortrait.getEntries()[index]);
+            return true;
+        } else if (preference == mRowsLandscape) {
+            intValue = Integer.parseInt((String) newValue);
+            index = mRowsLandscape.findIndexOfValue((String) newValue);
+            Settings.Secure.putInt(resolver,
+                    Settings.Secure.QS_ROWS_LANDSCAPE, intValue);
+            preference.setSummary(mRowsLandscape.getEntries()[index]);
+            return true;
+        } else if (preference == mQsColumns) {
+            intValue = Integer.parseInt((String) newValue);
+            index = mQsColumns.findIndexOfValue((String) newValue);
+            Settings.Secure.putInt(resolver,
+                    Settings.Secure.QS_COLUMNS, intValue);
+            preference.setSummary(mQsColumns.getEntries()[index]);
+            return true;
+        } else if (preference == mSysuiQqsCount) {
+            String SysuiQqsCount = (String) newValue;
+            int SysuiQqsCountValue = Integer.parseInt(SysuiQqsCount);
+            Settings.Secure.putInt(resolver, Settings.Secure.QQS_COUNT, SysuiQqsCountValue);
+            int SysuiQqsCountIndex = mSysuiQqsCount.findIndexOfValue(SysuiQqsCount);
+            mSysuiQqsCount.setSummary(mSysuiQqsCount.getEntries()[SysuiQqsCountIndex]);
+            return true;
+        } else if  (preference == mQsTileTitleVisibility) {
+            boolean checked = ((SwitchPreference) preference).isChecked();
+            Settings.System.putIntForUser(resolver,
+                    Settings.System.QS_TILE_TITLE_VISIBILITY, checked ? 1:0, UserHandle.USER_CURRENT);
+            return true;
         }
+        return false;
+    }
 
-        private void updateTileAnimationDurationSummary(int tileAnimationDuration) {
-            String prefix = (String) mTileAnimationDuration.getEntries()[mTileAnimationDuration.findIndexOfValue(String
-                    .valueOf(tileAnimationDuration))];
-            mTileAnimationDuration.setSummary(getResources().getString(R.string.qs_set_animation_duration, prefix));
-        }
+    private void updateTileAnimationStyleSummary(int tileAnimationStyle) {
+        String prefix = (String) mTileAnimationStyle.getEntries()[mTileAnimationStyle.findIndexOfValue(String
+                .valueOf(tileAnimationStyle))];
+        mTileAnimationStyle.setSummary(getResources().getString(R.string.qs_set_animation_style, prefix));
+    }
 
-        private void updateTileAnimationInterpolatorSummary(int tileAnimationInterpolator) {
-            String prefix = (String) mTileAnimationInterpolator.getEntries()[mTileAnimationInterpolator.findIndexOfValue(String
-                    .valueOf(tileAnimationInterpolator))];
-            mTileAnimationInterpolator.setSummary(getResources().getString(R.string.qs_set_animation_interpolator, prefix));
-        }
+    private void updateTileAnimationDurationSummary(int tileAnimationDuration) {
+        String prefix = (String) mTileAnimationDuration.getEntries()[mTileAnimationDuration.findIndexOfValue(String
+                .valueOf(tileAnimationDuration))];
+        mTileAnimationDuration.setSummary(getResources().getString(R.string.qs_set_animation_duration, prefix));
+    }
 
-        private void updateAnimTileStyle(int tileAnimationStyle) {
-            if (mTileAnimationDuration != null) {
-                if (tileAnimationStyle == 0) {
-                    mTileAnimationDuration.setSelectable(false);
-                    mTileAnimationInterpolator.setSelectable(false);
-                } else {
-                    mTileAnimationDuration.setSelectable(true);
-                    mTileAnimationInterpolator.setSelectable(true);
-                }
+    private void updateTileAnimationInterpolatorSummary(int tileAnimationInterpolator) {
+        String prefix = (String) mTileAnimationInterpolator.getEntries()[mTileAnimationInterpolator.findIndexOfValue(String
+                .valueOf(tileAnimationInterpolator))];
+        mTileAnimationInterpolator.setSummary(getResources().getString(R.string.qs_set_animation_interpolator, prefix));
+    }
+
+    private void updateAnimTileStyle(int tileAnimationStyle) {
+        if (mTileAnimationDuration != null) {
+            if (tileAnimationStyle == 0) {
+                mTileAnimationDuration.setSelectable(false);
+                mTileAnimationInterpolator.setSelectable(false);
+            } else {
+                mTileAnimationDuration.setSelectable(true);
+                mTileAnimationInterpolator.setSelectable(true);
             }
         }
     }
